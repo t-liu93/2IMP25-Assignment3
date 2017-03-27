4936fcaef0502d7f353b507051c4cb538c21d43f
Tianyu Liu <liu.tianyu93@hotmail.com>
2017-03-27 22:49:06 +0200

    1. SQL script to query the two parameters of review period, namely review time and commit time

---
+++ SqlScript/SQL_ReviewPeriod.sql
@@ -0,0 +1,8 @@
+/* Query the review period */
+/* Review period is the difference of t_reversion.rev_committedTime and t_change.ch_createdTime */
+select t_change.id as changeID, 
+	t_revision.id as revisionID, 
+    ch_createdTime as createdTime, 
+    rev_committedTime as committedTime
+from t_change, t_revision
+where t_change.id = t_revision.rev_changeId
