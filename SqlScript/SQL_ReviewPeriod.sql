d0a44c60fff34456567750521ddb627045452938
Tianyu Liu <liu.tianyu93@hotmail.com>
2017-03-27 22:53:22 +0200

    #Change idention

--- SqlScript/SQL_ReviewPeriod.sql
+++ SqlScript/SQL_ReviewPeriod.sql
@@ -1,7 +1,7 @@
 /* Query the review period */
 /* Review period is the difference of t_reversion.rev_committedTime and t_change.ch_createdTime */
 select t_change.id as changeID, 
-	t_revision.id as revisionID, 
+    t_revision.id as revisionID, 
     ch_createdTime as createdTime, 
     rev_committedTime as committedTime
 from t_change, t_revision
