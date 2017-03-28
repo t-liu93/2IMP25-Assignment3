8dd06bffbcd25b93354992c400c9afc9ac9f6e33
Tianyu Liu <liu.tianyu93@hotmail.com>
2017-03-28 17:46:44 +0200

    Optimize the sql script to query dependent variable also update the csv files of dependent variable

--- SqlScript/SQL_DependentVariable.sql
+++ SqlScript/SQL_DependentVariable.sql
@@ -2,7 +2,6 @@
 /* Review period is the difference of t_reversion.rev_committedTime and t_change.ch_createdTime */
 select t_change.id as changeID, 
     t_revision.id as revisionID, 
-    ch_createdTime as createdTime, 
-    rev_committedTime as committedTime
+    to_days(t_revision.rev_committedTime) - to_days(t_change.ch_createdTime) as ecosystemTenure
 from t_change, t_revision
 where t_change.id = t_revision.rev_changeId
