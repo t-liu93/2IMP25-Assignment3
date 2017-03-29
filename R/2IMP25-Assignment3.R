4eb5e25fa315d44571a34a3bd991ac80fdd2e347
xuxpp <xuxpp@vip.qq.com>
2017-03-29 22:03:43 +0200

    V0.2

--- R/2IMP25-Assignment3.R
+++ R/2IMP25-Assignment3.R
@@ -7,7 +7,7 @@
 library(outliers)
 par(mfrow = c(2, 2))
 #dep <- read.csv("D:\\2IMP25-Assignment3\\CSVdata\\Eclipse\\CSV_DependentVariable_Eclipse.csv")
-#ctrl <- read.csv("D:\\2IMP25-Assignment3\\CSVdata\\Eclipse\\CSV_ControlVariable_Eclipse.csv")
+ctrl <- read.csv("D:\\2IMP25-Assignment3\\CSVdata\\Eclipse\\CSV_ControlVariable_Eclipse.csv")
 dnc <- read.csv("D:\\2IMP25-Assignment3\\CSVdata\\Eclipse\\CSV_DependentAndControlVariable_Eclipse.csv")
 Pre <- read.csv("D:\\2IMP25-Assignment3\\CSVdata\\Eclipse\\CSV_PredictorVariable_Eclipse.csv")
 
@@ -23,6 +23,12 @@ rp_author <- c()
 rp_author[1] <- NA
 reduced_authorId <- c()
 reduced_authorId[1] <- NA
+LA_author <- c()
+LA_author[1] <- NA
+LD_author <- c()
+LD_author[1] <- NA
+FM_author <- c()
+FM_author[1] <- NA
 pre_author <- Pre$authorId
 h <- 1
 j <- 1
@@ -45,16 +51,24 @@ for(i in 1:length(dnc$reviewPeriod)){
 
 #new data dnc2
 dnc2 <- data.frame(authorId, changeId, revisionId, reviewPeriod, linesAdded, linesDeleted, numberOfFilesModified)
+#sort by authorId
 dnc2 <- dnc2[order(dnc2$authorId),]
 
-#Get reviewperiod per author
+#Get reviewperiod, Line added, Linedeleted and nr. files modified per author
 for(i in 1:(length(dnc2$authorId) - 1)) {
   if(is.na(rp_author[h])){ #new author
     rp_author[h] <- as.numeric(dnc2$reviewPeriod[i])
     reduced_authorId[h] <- dnc2$authorId[i]
+    LA_author[h] <- dnc2$linesAdded[i]
+    LD_author[h] <- dnc2$linesDeleted[i]
+    FM_author[h] <- dnc2$numberOfFilesModified[i]
   }
-  else
+  else {
     rp_author[h] <- as.numeric(rp_author[h]) + as.numeric(dnc2$reviewPeriod[i])
+    LA_author[h] <- LA_author[h] + dnc2$linesAdded[i]
+    LD_author[h] <- LD_author[h] + dnc2$linesDeleted[i]
+    FM_author[h] <- FM_author[h] + dnc2$numberOfFilesModified[i]
+  }
   
   if(dnc2$authorId[i] == dnc2$authorId[i+1]) { #next author is the same author
     h <- h
@@ -65,31 +79,77 @@ for(i in 1:(length(dnc2$authorId) - 1)) {
 }
 
 # new data with reviewperiod per author
-dnc3 = data.frame(reduced_authorId, rp_author)
-#get exp by info in pre
-exp <- 2*as.numeric(Pre$ecosystemTenure) + as.numeric(Pre$changeAcitivity) + 3*as.numeric(Pre$reviewTenure)
-+ as.numeric(Pre$reviewActivity) + 2*as.numeric(Pre$appBlockTenure) + as.numeric(Pre$appBlockActivity)
+dnc3 = data.frame(reduced_authorId, rp_author, LA_author, LD_author, FM_author)
 
-# exp per author
-exp_author <- data.frame(pre_author, exp)
-
-# find common author in RP/author and EXP/author  
+# find common author in pre and dnc3 
 h<-1
-exp_final <- c()
+#exp_final <- c()
 reviewPeriod_final <- c()
+author_final <- c()
+LA_final <- c()
+LD_final <- c()
+FM_final <- c()
+eco <- c()
+change <- c()
+review_tenure <- c()
+review_act <- c()
+block_tenure <- c()
+block_act <- c()
+
 for(i in 1:length(dnc3$reduced_authorId)) {
-  for(j in 1:length(exp_author$pre_author)) {
-    if(dnc3$reduced_authorId[i] == exp_author$pre_author[j]) {
-      exp_final[h] <- exp_author$exp[j]
+  for(j in 1:length(pre_author)) {
+    if(dnc3$reduced_authorId[i] == pre_author[j]) {
+      #exp_final[h] <- exp[j]
       reviewPeriod_final[h] <- dnc3$rp_author[i]
+      LA_final[h] <- dnc3$LA_author[i]
+      LD_final[h] <- dnc3$LD_author[i]
+      FM_final[h] <- dnc3$FM_author[i]
+      author_final[h] <- dnc3$reduced_authorId[i]
+      eco[h] <- Pre$ecosystemTenure[j]
+      change[h] <- Pre$changeAcitivity[j]
+      review_tenure[h] <- Pre$reviewTenure[j]
+      review_act[h] <- Pre$reviewActivity[j]
+      block_tenure[h] <- Pre$appBlockTenure[j]
+      block_act[h] <- Pre$appBlockActivity[j]
       h <- h + 1
     }
   }
 }
 
-model = lm(reviewPeriod_final ~ exp_final)
-  
+#data set with final LA, LD, FM, authorID, reviewPeriod
+dnc4 <- data.frame(author_final, LA_final, LD_final, FM_final, reviewPeriod_final, 
+             eco, change, review_tenure, review_act, block_tenure, block_act)
+
+#get exp 
+exp <- 2*as.numeric(dnc4$eco) + as.numeric(dnc4$change) + 3*as.numeric(dnc4$review_tenure) +
+  as.numeric(dnc4$review_act) + 2*as.numeric(dnc4$block_tenure) + as.numeric(dnc4$block_act) +
+  log(as.numeric(dnc4$LA_final)) + log(as.numeric(dnc4$LD_final)+0.5) + log(as.numeric(dnc4$FM_final))
+
+#final dataset
+dnc_final = data.frame(dnc4, exp)
+
+dnc_final_orer = dnc_final[order(exp),]
+
+#Make model
+model = lm(reviewPeriod_final ~ exp)
+
+#summary of the model  
 summary(model)
 plot(model)
+
+
+#eco <- c()
+#for(i in 1:200){
+#  eco[i] = Pre$ecosystemTenure[i]
+#}
+  
+#plot(eco)
+#qqnorm(eco)
+
+#plot(Pre$changeAcitivity)
+#plot(Pre$reviewTenure)
+#plot(Pre$reviewActivity)
+#plot(Pre$appBlockTenure)
+#plot(Pre$appBlockActivity)
 
 
